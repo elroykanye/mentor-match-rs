390c3f19221a68e30276e3b6a36dd0f462f715d3
elroykanye <elroykanye@gmail.com>
2021-09-18 20:23:35 +0100

    building mod tree: modified ./src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+mod db;
+mod model;
+mod service;
+
+#[macro_use] extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, World"
+}
+
+//noinspection ALL
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![index])
+}
+/*
 fn main() {
     println!("Hello, world!");
 }
+*/
+fn main() {}
